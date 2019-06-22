4e50a1cc667f134dad91945a2353eec693a37a73
Ian Ibbotson <ianibbo@googlemail.com>
2019-06-22 17:21:25 +0100

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# mod-wf-flowable
-Mod-workflow (flowable)
+
+
+See https://blog.flowable.org/2018/12/19/building-your-own-flowable-spring-boot-application/
---
+++ src/main/groovy/org/folio/modwfflowable/ModWfFlowableApplication.groovy
@@ -0,0 +1,13 @@
+package org.folio.modwfflowable
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ModWfFlowableApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ModWfFlowableApplication, args)
+	}
+
+}
