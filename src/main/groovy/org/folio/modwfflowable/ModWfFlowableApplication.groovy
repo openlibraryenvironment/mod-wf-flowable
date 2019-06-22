7b9abd62e7e71aa54cee625a1416818ff9699533
Ian Ibbotson <ianibbo@googlemail.com>
2019-06-22 18:21:47 +0100

    Jugging config

--- src/main/groovy/org/folio/modwfflowable/ModWfFlowableApplication.groovy
+++ src/main/groovy/org/folio/modwfflowable/ModWfFlowableApplication.groovy
@@ -2,12 +2,21 @@ package org.folio.modwfflowable
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.flowable.spring.SpringProcessEngineConfiguration;
+import org.flowable.spring.boot.EngineConfigurationConfigurer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 class ModWfFlowableApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(ModWfFlowableApplication, args)
-	}
+  static void main(String[] args) {
+    SpringApplication.run(ModWfFlowableApplication, args)
+  }
 
+  // @Bean
+  // public EngineConfigurationConfigurer<SpringProcessEngineConfiguration> customProcessEngineConfigurer() {
+  //   return engineConfiguration -> {
+  //     engineConfiguration.setValidateFlowable5EntitiesEnabled(false);
+  //   };
+  // }
 }
