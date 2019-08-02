f8c2575f2714ce2e1e6d708b850ad231ec697268
Ian Ibbotson <ianibbo@googlemail.com>
2019-08-02 13:48:04 +0100

    Fleshing out details on mt impl

--- src/main/groovy/org/folio/modwfflowable/TenantEndpoint.groovy
+++ src/main/groovy/org/folio/modwfflowable/TenantEndpoint.groovy
@@ -8,13 +8,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.flowable.engine.impl.cfg.multitenant.MultiSchemaMultiTenantProcessEngineConfiguration;
 
 @RestController
 public class ArticleWorkflowController {
 
+    @Autowired
+    MultiSchemaMultiTenantProcessEngineConfiguration processEngineConfiguration;
+
     @GetMapping("/_tenant")
     public String tenant() {
       println("Tenant post mapping");
+
+      // as per https://blog.flowable.org/2018/09/11/multitenancy-in-flowable/
+      // processEngineConfiguration.registerTenant("new-tenant", createDataSource("jdbc:h2:mem:flowable-new-tenant;DB_CLOSE_DELAY=1000", "sa", ""));
       return "Hello"
     }
   
